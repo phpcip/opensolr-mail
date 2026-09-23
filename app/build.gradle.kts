62eb2e68340a7c1e3a69ca04e568c81bfe2ff721
Ciprian Dimofte <cip@opensolr.com>
2026-09-23 10:58:15 +0300

    1.5.3: reorderable folders under All accounts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.opensolr.mail"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "1.5.2"
+        versionCode = 28
+        versionName = "1.5.3"
     }
 
     flavorDimensions += "store"
