955fb5c2d84fe573945588882cd9148247126506
Ciprian Dimofte <cip@opensolr.com>
2026-09-24 23:12:18 +0300

    Release 1.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.opensolr.mail"
         minSdk = 26
         targetSdk = 36
-        versionCode = 45
-        versionName = "1.9.1"
+        versionCode = 46
+        versionName = "1.10.0"
     }
 
     flavorDimensions += "store"
