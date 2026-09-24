84ef206c5ccf93e7d48ae02a7aa342322ef46d14
Ciprian Dimofte <cip@opensolr.com>
2026-09-24 10:29:25 +0300

    1.8.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.opensolr.mail"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "1.8.9"
+        versionCode = 43
+        versionName = "1.8.10"
     }
 
     flavorDimensions += "store"
